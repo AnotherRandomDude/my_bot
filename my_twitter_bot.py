462bd6523cec47aa815946cc10cea3cc64f3fcce
WALTER ALEJANDRO LAM ASTUDILLO <WALAMAST@uwaterloo.ca>
2020-05-15 15:48:03 -0400

    every minute

--- my_twitter_bot.py
+++ my_twitter_bot.py
@@ -4,7 +4,6 @@ import time
 import os
 from os import environ
 
-interval = 60     #interval
 consumer_key = environ['CONSUMER_KEY']
 consumer_secret_key = environ['CONSUMER_SECRET_KEY']
 access_token = environ['ACCESS_TOKEN']
@@ -24,8 +23,4 @@ while (True):
 
         tbody = f'------------------------ CANADA ---------------------------- \n{currentMonth} {currentYear} \nEnjoy $5.00 Off Your First Order With Skip The Dishes! \nClick my referral Link: http://skipthedishes.com/r/PZ01zaXhrU \n#skipthedishes \n#skipthedishescoupon \n#helpauniversitystudentgetfood \nThanks'
         api.update_with_media("skip.jpg", tbody)
-        time.sleep(interval)
-    
-    else
-        
-        time.sleep(interval * 4)
+        time.sleep(60)
